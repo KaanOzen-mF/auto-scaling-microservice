445f45f0ce10bd66b8b69cd7713f7f1c5cdd260b
KaanOzen-mF <kaanozn98@gmail.com>
2025-05-26 11:03:13 +0300

    add product deletion and editing functionality with UI updates

--- auto-scaling-microservice-frontend/src/app/products/new/page.tsx
+++ auto-scaling-microservice-frontend/src/app/products/new/page.tsx
@@ -42,7 +42,7 @@ export default function NewProductPage() {
   const [error, setError] = useState<string | null>(null);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-  const PRODUCT_SERVICE_BASE_API_URL = "http://127.0.0.1:51493/products";
+  const PRODUCT_SERVICE_BASE_API_URL = "http://127.0.0.1:51674/products";
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -123,12 +123,12 @@ export default function NewProductPage() {
   };
 
   return (
-    <main className="container mx-auto p-4">
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold">Add new product</h1>
+    <main className="container mx-auto p-4 ">
+      <div className="flex justify-between items-center mb-6 ">
+        <h1 className="text-3xl font-bold">Add new product</h1>
         <Link
           href="/"
-          className="text-blue-500 hover:text-blue-700 hover:underline"
+          className="text-white text-2xl hover:text-amber-400 hover:underline"
         >
           &larr; Back to Products
         </Link>
@@ -145,12 +145,12 @@ export default function NewProductPage() {
 
       <form
         onSubmit={handleSubmit}
-        className="space-y-6 max-w-2xl mx-auto p-6 shadow-xl rounded-lg border border-gray-200"
+        className="space-y-6 max-w-2xl mx-auto p-6 rounded-lg backdrop-blur-m bg-gray-600"
       >
         <div>
           <label
             htmlFor="name"
-            className="block text-sm font-medium text-gray-700"
+            className="block text-sm font-medium text-shadow-white"
           >
             Product Name <span className="text-red-500">*</span>
           </label>
@@ -161,13 +161,14 @@ export default function NewProductPage() {
             value={formData.name}
             onChange={handleChange}
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="mt-1 block w-full px-3 py-2 bg-white/4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:bg-white/20 focus:border-amber-400 transition-colors duration-200"
           />
         </div>
         <div>
           <label
             htmlFor="description"
-            className="block text-sm font-medium text-gray-700"
+            className="block text-sm font-medium text-shadow-white"
           >
             Short Description <span className="text-red-500">*</span>
           </label>
@@ -178,13 +179,14 @@ export default function NewProductPage() {
             onChange={handleChange}
             required
             rows={2}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="mt-1 block w-full px-3 py-2 bg-white/4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:bg-white/20 focus:border-amber-400 transition-colors duration-200"
           ></textarea>
         </div>
         <div>
           <label
             htmlFor="detailedDescription"
-            className="block text-sm font-medium text-gray-700"
+            className="block text-sm font-medium text-shadow-white"
           >
             Long Description
           </label>
@@ -194,13 +196,14 @@ export default function NewProductPage() {
             value={formData.detailedDescription || ""}
             onChange={handleChange}
             rows={4}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="mt-1 block w-full px-3 py-2 bg-white/4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:bg-white/20 focus:border-amber-400 transition-colors duration-200"
           ></textarea>
         </div>
         <div>
           <label
             htmlFor="imageUrl"
-            className="block text-sm font-medium text-gray-700"
+            className="block text-sm font-medium text-shadow-white"
           >
             Image URL <span className="text-red-500">*</span>
           </label>
@@ -211,14 +214,15 @@ export default function NewProductPage() {
             value={formData.imageUrl}
             onChange={handleChange}
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="mt-1 block w-full px-3 py-2 bg-white/4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:bg-white/20 focus:border-amber-400 transition-colors duration-200"
           />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
             <label
               htmlFor="price"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-shadow-white"
             >
               Fiyat <span className="text-red-500">*</span>
             </label>
@@ -231,13 +235,14 @@ export default function NewProductPage() {
               required
               step="0.01"
               min="0"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 bg-white/4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:bg-white/20 focus:border-amber-400 transition-colors duration-200"
             />
           </div>
           <div>
             <label
               htmlFor="stockQuantity"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-shadow-white"
             >
               Stock Quantity <span className="text-red-500">*</span>
             </label>
@@ -250,14 +255,15 @@ export default function NewProductPage() {
               required
               step="1"
               min="0"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 bg-white/4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:bg-white/20 focus:border-amber-400 transition-colors duration-200"
             />
           </div>
         </div>
         <div>
           <label
             htmlFor="category"
-            className="block text-sm font-medium text-gray-700"
+            className="block text-sm font-medium text-shadow-white"
           >
             Category <span className="text-red-500">*</span>
           </label>
@@ -268,13 +274,15 @@ export default function NewProductPage() {
             value={formData.category}
             onChange={handleChange}
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="mt-1 block w-full px-3 py-2 bg-white/4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:bg-white/20 focus:border-amber-400 transition-colors duration-200"
           />
         </div>
         <button
           type="submit"
           disabled={isSubmitting}
-          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:bg-gray-300"
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-900 
+          hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2  disabled:bg-gray-300 hover:cursor-pointer"
         >
           {isSubmitting ? "Adding..." : "Add New Product"}
         </button>
