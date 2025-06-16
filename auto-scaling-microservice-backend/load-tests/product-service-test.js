c5ccb8a96d33b965d436e17d5a6cbb168133e2f0
KaanOzen-mF <kaanozn98@gmail.com>
2025-06-16 13:47:33 +0300

    feat(tests): add load test for product service using k6

---
+++ auto-scaling-microservice-backend/load-tests/product-service-test.js
@@ -0,0 +1,29 @@
+// load-tests/product-service-test.js
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+  // stages: [
+  //   { duration: '30s', target: 20 },
+  //   { duration: '1m', target: 20 },
+  //   { duration: '10s', target: 0 },
+  // ],
+  vus: 10,
+  duration: "30s",
+  thresholds: {
+    http_req_failed: ["rate<0.01"],
+    http_req_duration: ["p(95)<500"],
+  },
+};
+
+export default function () {
+  const productServiceUrl = "http://127.0.0.1:51224/products"; // minikube service product-service --url
+
+  const res = http.get(productServiceUrl);
+
+  check(res, {
+    "status was 200": (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
