00bd2ec7bda098fe1fbbe19820eefefde4f79db9
KaanOzen-mF <kaanozn98@gmail.com>
2025-06-16 17:04:47 +0300

    feat(tests): add comprehensive load testing script for product service

---
+++ auto-scaling-microservice-backend/load-tests/comprehensive-test.js
@@ -0,0 +1,54 @@
+// load-tests/comprehensive-test.js
+import http from "k6/http";
+import { check, sleep, group } from "k6";
+import { Trend } from "k6/metrics";
+
+// --- Test Options ---
+export const options = {
+  stages: [
+    { duration: "30s", target: 20 }, // 30 seconds 20 user
+    { duration: "1m", target: 20 }, // 1 minute 20 user
+    { duration: "30s", target: 50 }, // 30 seconds 50 user
+    { duration: "2m", target: 50 }, // 2 minutes 50 user
+    { duration: "1m", target: 0 }, // 1 minute 0 user
+  ],
+  thresholds: {
+    http_req_failed: ["rate<0.01"],
+    http_req_duration: ["p(95)<800"], // Let's aim to keep the 95th percentile below 800ms even at peak load
+    "http_req_duration{group:::list_products}": ["p(95)<600"], // Expect product listing to be faster
+    "http_req_duration{group:::get_one_product}": ["p(90)<400"], // Single product retrieval should be even faster
+  },
+};
+
+const BASE_URL = "http://127.0.0.1:51224"; // `minikube service product-service --url`
+
+const sampleProductIds = [
+  "2500b4a3-727e-44ed-9a67-b7a007747d31", // laptop
+  "372e230d-096e-45a4-8b78-92badb0364e4", // wireless mouse
+];
+
+// --- Test Scenarios ---
+export default function () {
+  // 70% of users list products, 30% look at a single product
+
+  group("list_products", function () {
+    // Request to list all products
+    const res = http.get(`${BASE_URL}/products?page=1&limit=10`);
+    check(res, { "status is 200 (list)": (r) => r.status === 200 });
+  });
+
+  sleep(1); // Wait between requests
+
+  if (sampleProductIds.length > 0) {
+    group("get_one_product", function () {
+      // Pick a random product ID from the sample list
+      const productId =
+        sampleProductIds[Math.floor(Math.random() * sampleProductIds.length)];
+
+      const res = http.get(`${BASE_URL}/products/${productId}`);
+      check(res, { "status is 200 (get one)": (r) => r.status === 200 });
+    });
+  }
+
+  sleep(2); // Wait longer between iterations
+}
